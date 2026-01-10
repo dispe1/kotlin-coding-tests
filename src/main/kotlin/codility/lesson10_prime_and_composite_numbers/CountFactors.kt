d26f947151a38ce20acdf7a838e13877131784c4
Gichan Im <lkjim0757@naver.com>
2026-01-10 23:36:20 +0900

    Clarify CountFactors loop variable types

--- src/main/kotlin/codility/lesson10_prime_and_composite_numbers/CountFactors.kt
+++ src/main/kotlin/codility/lesson10_prime_and_composite_numbers/CountFactors.kt
@@ -4,15 +4,15 @@ class CountFactors {
     fun solution(N: Int): Int {
         if (N <= 0) return 0
 
-        var result = 0
-        var i = 1
         val target = N.toLong()
+        var result = 0
+        var i = 1L
 
-        while (i.toLong() * i < target) {
-            if (N % i == 0) result += 2
+        while (i * i < target) {
+            if (N % i.toInt() == 0) result += 2
             i++
         }
-        if (i.toLong() * i == target) result++
+        if (i * i == target) result++
 
         return result
     }
