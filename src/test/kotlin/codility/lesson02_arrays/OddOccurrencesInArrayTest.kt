9b55da10b6fdd3b770e348090d4f4d2253948f98
Gichan Im <lkjim0757@naver.com>
2026-01-10 15:45:14 +0900

    Add stress tests for OddOccurrencesInArray

--- src/test/kotlin/codility/lesson02_arrays/OddOccurrencesInArrayTest.kt
+++ src/test/kotlin/codility/lesson02_arrays/OddOccurrencesInArrayTest.kt
@@ -41,4 +41,38 @@ class OddOccurrencesInArrayTest {
         val result = solver.solution(intArrayOf(5, 5, 1, 2, 2))
         assertEquals(1, result)
     }
+
+    @Test
+    fun testLargeArrayWithSamePairValues() {
+        // Construct array: 100,000 ones + one 2
+        // Size: 100,001 (odd)
+        // [1, 1, ..., 1, 2]
+        val n = 100000
+        val arr = IntArray(n + 1)
+        for (i in 0 until n) {
+            arr[i] = 1
+        }
+        arr[n] = 2 // The unique element
+
+        assertEquals(2, solver.solution(arr))
+    }
+
+    @Test
+    fun testExtremeLargeArray() {
+        // Construct array: 999,998 elements (499,999 pairs) + 1 unique
+        // Using distinct pairs to stress test XOR
+        // Pairs: 0,0, 1,1, 2,2, ...
+        val pairsCount = 499999
+        val arr = IntArray(pairsCount * 2 + 1)
+        
+        for (i in 0 until pairsCount) {
+            arr[2 * i] = i
+            arr[2 * i + 1] = i
+        }
+        // Unique element at the end
+        val uniqueVal = 999999
+        arr[arr.size - 1] = uniqueVal
+
+        assertEquals(uniqueVal, solver.solution(arr))
+    }
 }
